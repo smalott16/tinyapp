227fe81c65be05863b6586fc78c02aa919ac78f0
Spencer Malott <spencermalott16@gmail.com>
2021-09-27 22:03:30 +0000

    added the urls_index view

--- express_server.js
+++ express_server.js
@@ -7,14 +7,19 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-})
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
@@ -22,5 +27,5 @@ app.get("/hello", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
