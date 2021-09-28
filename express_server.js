52366ff5bab628f6ea378dc0f260ebad547cb285
Spencer Malott <spencermalott16@gmail.com>
2021-09-28 03:31:39 +0000

    added the feature that redirects to the long url, aka the actual website

--- express_server.js
+++ express_server.js
@@ -58,6 +58,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 })
 
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params["shortURL"]])
+})
+
   // app.get("/hello", (req, res) => {
   //   res.send("<html><body>Hello <b>World</b></body></html>\n");
   // });
