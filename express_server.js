b9a7aeba4f1c576b55e8467e751e38ae913dac78
Spencer Malott <spencermalott16@gmail.com>
2021-09-28 02:31:17 +0000

    added a generate random string function to the express server file

--- express_server.js
+++ express_server.js
@@ -10,6 +10,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 ///////////////////////////////////////////////////////
 
+const generateRandomString = function () {
+  const charactersForString = "0123456789abcdefghijklmnopqrstuvwxyz".split("");
+  let randomIDArray = [];
+  for (let i = 1; i<=6; i++) {
+    randomIDArray.push(charactersForString[Math.floor(Math.random() * (charactersForString.length - 1))]);
+  }
+  return randomIDArray.join("");
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -36,6 +45,11 @@ app.get("/urls/:shortURL", (req, res) => {
   }
   res.render("urls_show", templateVars);
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
   // app.get("/hello", (req, res) => {
   //   res.send("<html><body>Hello <b>World</b></body></html>\n");
