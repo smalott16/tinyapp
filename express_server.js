7daec78d536c60d6333cfbbec7a9733890ec55d8
Spencer Malott <spencermalott16@gmail.com>
2021-09-28 03:21:29 +0000

    updated express server to save the newly added urls to the internal urlDatabase object so it is accessible to the my URLs tab. Seems to work fine at this point

--- express_server.js
+++ express_server.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 ///////////////////////////////////////////////////////
 
 const generateRandomString = function () {
-  const charactersForString = "0123456789abcdefghijklmnopqrstuvwxyz".split("");
+  const charactersForString = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   let randomIDArray = [];
   for (let i = 1; i<=6; i++) {
     randomIDArray.push(charactersForString[Math.floor(Math.random() * (charactersForString.length - 1))]);
@@ -38,18 +38,25 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  //res.send("ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body["longURL"]
+  console.log(urlDatabase);
+
+  res.redirect(`/urls/${shortURL}`);
+  
+});
+
 app.get("/urls/:shortURL", (req, res) => {
+  //console.log(req);
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   }
   res.render("urls_show", templateVars);
 })
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
 
   // app.get("/hello", (req, res) => {
   //   res.send("<html><body>Hello <b>World</b></body></html>\n");
